cf78394914d3429b1a76efd51923d1a95ffaafac
A_Kub_work <photokub@yandex.ru>
2023-03-06 18:57:16 +0300

    Feat: создание компонента с сохранёнными фильмами

---
+++ movies-explorer/src/components/SavedMovies/SavedMovies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+export default function SavedMovies() {
+    return (
+      <>
+          <MoviesCardList/>
+          <MoviesCard/>
+      </>
+    )
+}
