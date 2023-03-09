62c62011acad8074d5705150e304ffdc720d5010
A_Kub_work <photokub@yandex.ru>
2023-03-09 16:05:43 +0300

    Feat: верстка AboutProject

--- movies-explorer/src/components/Main/Main.jsx
+++ movies-explorer/src/components/Main/Main.jsx
@@ -5,6 +5,7 @@ import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
 import NavTab from "../NavTab/NavTab";
+import './Main.css'
 
 export default function Main() {
     return (
