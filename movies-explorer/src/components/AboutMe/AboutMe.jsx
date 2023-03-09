2d792cac0b182de211bae4e9f62af377ce4f2912
Photokub <photokub@yandex.ru>
2023-03-09 23:15:08 +0300

    Feat: верстка AboutMe

--- movies-explorer/src/components/AboutMe/AboutMe.jsx
+++ movies-explorer/src/components/AboutMe/AboutMe.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import photo from '../../images/pic1.png'
+import './AboutMe.css'
+import {Link} from "react-router-dom";
 
 export default function AboutMe() {
     return (
-        <section>
-            <p>AboutMe</p>
+        <section className='about-me main__section'>
+            <h2 className='about-me__title main__title'>Студент</h2>
+            <h4 className='about-me__subtitle main__subhead'>Александр</h4>
+            <h5 className='about-me__caption'>Фронтенд-разработчик, 36 лет</h5>
+            <p className='about-me__text main__text'>Родился и живу в Москве, где и учился в колледже на факультете «ЭВМ, комплексы и сети», а затем получил образование в институте и аспирантуре на факультете дизайна. Работаю дизайнером на протяжении 11 лет, 6 из которых занимаюсь веб-дизайном. На курсах веб-разработки прочакачал свои скилы и теперь выполняю заказы «под ключь» – от макета до разработки. Увлекаюсь рыбалкой, горными лыжами, резьбой по дереву. Женат на красивой жене.</p>
+            <img className='about-me__photo' src={photo} alt='фото Александра'/>
+            <Link className='about-me__github-link' to='https://github.com/Photokub'>Github</Link>
         </section>
     )
 }
--- movies-explorer/src/components/Header/Header.jsx
+++ movies-explorer/src/components/Header/Header.jsx
@@ -6,7 +6,9 @@ import './Header.css'
 export default function Header() {
     return (
         <header className="header">
+            <Link className="header__logo-link" to="/">
             <img className="header__logo" src={logo} alt="логотип movies explorer"/>
+            </Link>
             <div className='header__container'>
                 <Link className="header__link" to="#">Регистрация</Link>
                 <button className='header__button' type='button'>Войти</button>
