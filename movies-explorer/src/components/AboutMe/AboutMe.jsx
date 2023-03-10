2280d17370cbd46fafdf22130eae258a29e5f30f
Photokub <photokub@yandex.ru>
2023-03-10 19:38:54 +0300

    Feat: верстка Portfolio

--- movies-explorer/src/components/AboutMe/AboutMe.jsx
+++ movies-explorer/src/components/AboutMe/AboutMe.jsx
@@ -7,11 +7,17 @@ export default function AboutMe() {
     return (
         <section className='about-me main__section'>
             <h2 className='about-me__title main__title'>Студент</h2>
-            <h4 className='about-me__subtitle main__subhead'>Александр</h4>
-            <h5 className='about-me__caption'>Фронтенд-разработчик, 36 лет</h5>
-            <p className='about-me__text main__text'>Родился и живу в Москве, где и учился в колледже на факультете «ЭВМ, комплексы и сети», а затем получил образование в институте и аспирантуре на факультете дизайна. Работаю дизайнером на протяжении 11 лет, 6 из которых занимаюсь веб-дизайном. На курсах веб-разработки прочакачал свои скилы и теперь выполняю заказы «под ключь» – от макета до разработки. Увлекаюсь рыбалкой, горными лыжами, резьбой по дереву. Женат на красивой жене.</p>
-            <img className='about-me__photo' src={photo} alt='фото Александра'/>
-            <Link className='about-me__github-link' to='https://github.com/Photokub'>Github</Link>
+            <div className='about-me__container'>
+                <h4 className='about-me__subtitle main__subhead'>Александр</h4>
+                <h5 className='about-me__caption'>Фронтенд-разработчик, 36 лет</h5>
+                <p className='about-me__text main__text'>Родился и живу в Москве, где и учился в колледже на факультете
+                    «ЭВМ, комплексы и сети», а затем получил образование в институте и аспирантуре на факультете
+                    дизайна. Работаю дизайнером на протяжении 11 лет, 6 из которых занимаюсь веб-дизайном. На курсах
+                    веб-разработки прочакачал свои скилы и теперь выполняю заказы «под ключь» – от макета до разработки.
+                    Увлекаюсь рыбалкой, горными лыжами, резьбой по дереву. Женат на красивой жене.</p>
+                <img className='about-me__photo' src={photo} alt='фото Александра'/>
+                <Link className='about-me__github-link' to='https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow'>Github</Link>
+            </div>
         </section>
     )
 }
--- movies-explorer/src/components/Portfolio/Portfolio.jsx
+++ movies-explorer/src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import './Portfolio.css'
 
 export default function Portfolio() {
     return (
-        <section>
-            <p>Portfolio</p>
+        <section className='portfolio'>
+            <h6 className="portfolio__title">Портфолио</h6>
+            <ul className="portfolio__list">
+                <li className="portfolio__list__string">Статичный сайт</li>
+                <li className="portfolio__list__string">Адаптивный сайт</li>
+                <li className="portfolio__list__string">Одностраничное приложение</li>
+            </ul>
         </section>
     )
 }
