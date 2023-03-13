9d534d659e8174752b9e322faf4de58965d6f18f
A_Kub_work <photokub@yandex.ru>
2023-03-13 18:55:03 +0300

    Feat: создание Preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import './Preloader.css'
 
 export default function Preloader() {
     return (
-        <section>
-
+        <section className="preloader">
+            <div className="preloader__container">
+                <span className="preloader__round"></span>
+            </div>
         </section>
     )
 }
