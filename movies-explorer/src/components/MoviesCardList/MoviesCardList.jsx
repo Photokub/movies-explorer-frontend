2076afed2661dbba156818044f83726c24409697
A_Kub_work <photokub@yandex.ru>
2023-03-13 12:20:14 +0300

    Refactor: перенос всех файлов проекта на дирректорию выше

--- movies-explorer/src/components/MoviesCardList/MoviesCardList.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MoviesCardList() {
-    return (
-        <section>
-
-        </section>
-    )
-}
--- movies-explorer/src/components/Register/Register.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Register() {
-    return (
-        <section>
-            <p>Register</p>
-        </section>
-    )
-}
