de726f966ca66a4b6fd0b4fe440872c8152d10ce
A_Kub_work <photokub@yandex.ru>
2023-03-14 13:03:13 +0300

    Refactor: правка чекбокса сохранённых фильмов

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import './MoviesCard.css'
 import image from '../../images/pic__COLOR_pic.png'
-import saved_enabled from '../../images/save9.svg'
-import saved_disabled from '../../images/save9d.svg'
 
 export default function MoviesCard() {
     return (
@@ -10,8 +8,10 @@ export default function MoviesCard() {
             <div className='movies-card-element__label'>
                 <p className='movies-card-element__label__title'>33 слова о дизайне</p>
                 <p className='movies-card-element__label__subtitle'>1ч 47м</p>
-                <img className='movies-card-element__label__enabled' src={saved_enabled} alt='фильм сохранен'/>
-                {/*<img className='movies-card-element__label__disabled' src={saved_disabled} alt='фильм не сохранен'/>*/}
+                <label className='save-checkbox'>
+                    <input className='save-checkbox__switcher' type='checkbox'/>
+                    <span className='save-checkbox__point' />
+                </label>
             </div>
             <img className='movies-card-element__image' src={image} alt='картинка фильма'/>
         </section>
