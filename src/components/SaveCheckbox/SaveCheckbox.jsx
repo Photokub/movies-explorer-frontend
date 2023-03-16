e621a968e10b5a4de0c0e937ba9c8fd44ac9f893
A_Kub_work <photokub@yandex.ru>
2023-03-16 12:09:35 +0300

    Feat: создание кнопки удаления карточки

--- src/components/SaveCheckbox/SaveCheckbox.jsx
+++ src/components/SaveCheckbox/SaveCheckbox.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import './SaveCheckbox.css'
+import {useLocation} from "react-router-dom";
 
 export default function SaveCheckbox() {
+    const location = useLocation()
+
     return(
         <label className='save-checkbox'>
             <input className='save-checkbox__switcher' type='checkbox'/>
-            <span className='save-checkbox__point' />
+            <span className={ location.pathname='/saved-movies' ?  'save-checkbox__delete-point' : 'save-checkbox__point' } />
         </label>
     )
 }
