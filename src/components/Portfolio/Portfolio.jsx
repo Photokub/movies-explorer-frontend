58cc0d15672057d711cf5aefcf224fa1bb527bb6
Photokub <photokub@yandex.ru>
2023-03-18 11:55:11 +0300

    Fix: Преобразование portfolio__list__string в единую ссылку

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,9 +6,9 @@ export default function Portfolio() {
         <section className='portfolio'>
             <h6 className="portfolio__title">Портфолио</h6>
             <ul className="portfolio__list">
-                <li className="portfolio__list__string">Статичный сайт<a className='portfolio__list__link' href='https://photokub.github.io/russian-travel/' target='_blank' rel="noreferrer">↗</a></li>
-                <li className="portfolio__list__string">Адаптивный сайт<a className='portfolio__list__link' href='https://photokub.github.io/russian-travel/' target='_blank' rel="noreferrer">↗</a></li>
-                <li className="portfolio__list__string">Одностраничное приложение<a className='portfolio__list__link' href='https://photokub.github.io/russian-travel/' target='_blank' rel="noreferrer">↗</a></li>
+                <li className="portfolio__list__string"><a className='portfolio__list__link' href='https://photokub.github.io/russian-travel/' target='_blank' rel="noreferrer">Статичный сайт</a></li>
+                <li className="portfolio__list__string"><a className='portfolio__list__link' href='https://photokub.github.io/russian-travel/' target='_blank' rel="noreferrer">Адаптивный сайт</a></li>
+                <li className="portfolio__list__string"><a className='portfolio__list__link' href='https://photokub.github.io/russian-travel/' target='_blank' rel="noreferrer">Одностраничное приложение</a></li>
             </ul>
         </section>
     )
