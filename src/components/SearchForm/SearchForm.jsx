b12ae3cfc894c921bab33a6b63152051258ca2f0
Photokub <photokub@yandex.ru>
2023-03-19 16:12:34 +0300

    Feat: интекрактивная граница инпута поиска

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useRef, useState} from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import './SearchForm.css'
 
 export default function SearchForm() {
+    const searchFormBorder = useRef(null)
+    const searchFormInput = useRef(null)
+    const borderBlur = '1.5px solid rgba(100, 100, 100, .2)'
+    const borderFocus = '1.5px solid rgba(100, 100, 100, .8)'
+
+    const borderStyle = {border: borderBlur}
+    const [formBorder, setFormBorder] = useState(borderStyle)
+
+    const handleFocus = (searchFormInput) => {
+        (document.activeElement === searchFormInput) && setFormBorder({border: borderFocus});
+
+    };
+
+    const handleBlur = (searchFormInput) => {
+        searchFormInput && setFormBorder({border: borderBlur});
+    };
+
+
     return (
         <div className='search-form-container'>
-            <form className='search-form'>
-                <input className='search-form__input' type='text' placeholder='Фильм'/>
+            <form className='search-form' ref={searchFormBorder} style={formBorder}>
+                <input
+                    className='search-form__input'
+                    type='text' placeholder='Фильм'
+                    ref={searchFormInput}
+                    onFocus={() => handleFocus(searchFormInput.current)}
+                    onBlur={handleBlur}/>
                 <button className='search-form__button' type='submit'>Поиск</button>
                 <FilterCheckbox/>
             </form>
