dd303f920bd80baca78808e96f5548d763bcdd33
Photokub <photokub@yandex.ru>
2023-03-21 00:10:34 +0300

    Feat: функционал формы поиска

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState} from "react";
+import React, {useRef, useState, useEffect} from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+import {moviesApi} from '../../utils/MoviesApi'
 import './SearchForm.css'
 
 export default function SearchForm() {
@@ -7,6 +8,61 @@ export default function SearchForm() {
     const searchFormInput = useRef(null)
     const borderBlur = '1.5px solid rgba(100, 100, 100, .2)'
     const borderFocus = '1.5px solid rgba(100, 100, 100, .8)'
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [beatfilmsArr, setBeatfilmsArr] = useState([])
+
+    // useEffect(() => {
+    //         Promise.all([moviesApi.getMovies()])
+    //             .then(([data]) => {
+    //                 {
+    //                     setBeatfilmsArr(data)
+    //                 }
+    //             })
+    //             .catch((err) => {
+    //                 console.log(`Ошибка ${err}`)
+    //             })
+    //
+    // }, []);
+
+    //console.log(beatfilmsArr)
+
+    const getbeatfilmMovies = () => {
+        moviesApi.getMovies()
+            .then(data => {
+                setBeatfilmsArr(data)
+            }).catch((err) => {
+            console.log(`Ошибка ${err}`)
+        })
+    }
+
+
+    const handleChange = event => {
+        setSearchTerm(event.target.value);
+        console.log(searchTerm)
+    };
+
+    const handleSearchValue = (e) => {
+        e.preventDefault()
+        getbeatfilmMovies()
+        console.log(beatfilmsArr)
+         console.log(searchTerm)
+        const results =  beatfilmsArr.forEach((film) => film.nameRU.includes(searchTerm))
+        return results
+        // setSearchTerm(e.target.value)
+    }
+
+
+    // useEffect(() => {
+    //     getbeatfilmMovies()
+    //     console.log(beatfilmsArr)
+    //     const results =  beatfilmsArr.filter(film => film.includes(searchTerm))
+    //     //     .filter(film =>
+    //     //     film.toLowerCase().includes(searchTerm)
+    //     // );
+    //     setSearchResults(results);
+    //     console.log(searchResults)
+    // }, [searchTerm]);
 
     const borderStyle = {border: borderBlur}
     const [formBorder, setFormBorder] = useState(borderStyle)
@@ -22,16 +78,18 @@ export default function SearchForm() {
 
     return (
         <div className='search-form-container'>
-            <form className='search-form' ref={searchFormBorder} style={formBorder} >
+            <form className='search-form' ref={searchFormBorder} style={formBorder} onSubmit={handleSearchValue}>
                 <input
                     required
                     className='search-form__input'
                     type='text'
                     placeholder='Фильм'
+                    value={searchTerm}
                     ref={searchFormInput}
+                    onChange={handleChange}
                     onFocus={() => handleFocus(searchFormInput.current)}
                     onBlur={handleBlur}/>
-                <button className='search-form__button' type='submit'>Поиск</button>
+                <button className='search-form__button' type='submit' >Поиск</button>
                 <FilterCheckbox/>
             </form>
             <hr className='search-form-container__border'></hr>
